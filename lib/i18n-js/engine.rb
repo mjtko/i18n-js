a1350a7f7e3b80e8d2a90dab15e354c6afe8b883
Mark J. Titorenko <mark.titorenko@alces-software.com>
2011-10-18 10:22:26 +0100

    Don't fall over if the assets pipeline has been disabled (references #57)

--- lib/i18n-js/engine.rb
+++ lib/i18n-js/engine.rb
@@ -5,27 +5,31 @@ module SimplesIdeias
       I18N_TRANSLATIONS_ASSET = 'i18n/translations'
 
       initializer 'i18n-js.asset_dependencies', :after => "sprockets.environment" do
-        config = Rails.root.join('config','i18n-js.yml')
-        cache_file = I18n::Engine.load_path_hash_cache
-        Rails.application.assets.register_preprocessor 'application/javascript', :'i18n-js_dependencies' do |ctx,data|
-          if ctx.logical_path == I18N_TRANSLATIONS_ASSET
-            ctx.depend_on(config)
-            # also set up dependencies on every locale file
-            ::I18n.load_path.each {|path| ctx.depend_on(path)}
-            # Set up a dependency on the contents of the load path
-            # itself.  In some situations it is possible to get here
-            # before the path hash cache file has been written; in
-            # this situation, write it now.
-            I18n::Engine.write_hash! unless File.exists?(cache_file)
-            ctx.depend_on(cache_file)
+        if Rails.application.assets
+          config = Rails.root.join('config','i18n-js.yml')
+          cache_file = I18n::Engine.load_path_hash_cache
+          Rails.application.assets.register_preprocessor 'application/javascript', :'i18n-js_dependencies' do |ctx,data|
+            if ctx.logical_path == I18N_TRANSLATIONS_ASSET
+              ctx.depend_on(config)
+              # also set up dependencies on every locale file
+              ::I18n.load_path.each {|path| ctx.depend_on(path)}
+              # Set up a dependency on the contents of the load path
+              # itself.  In some situations it is possible to get here
+              # before the path hash cache file has been written; in
+              # this situation, write it now.
+              I18n::Engine.write_hash! unless File.exists?(cache_file)
+              ctx.depend_on(cache_file)
+            end
+            data
           end
-          data
         end
       end
 
       # rewrite path cache hash at startup and before each request in development
       config.to_prepare do
-        SimplesIdeias::I18n::Engine.write_hash_if_changed unless Rails.env.production?
+        if Rails.application.assets
+          SimplesIdeias::I18n::Engine.write_hash_if_changed unless Rails.env.production?
+        end
       end
 
       class << self
